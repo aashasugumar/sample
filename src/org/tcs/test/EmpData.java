3f98653278f1596450b587d191c7be72f4a18834
aashasugumar <aashajyothis@gmail.com>
2022-09-22 16:08:12 +0530

    new code

---
+++ src/org/tcs/test/EmpData.java
@@ -0,0 +1,35 @@
+package org.tcs.test;
+
+
+public class EmpData {
+public void empDetail() {
+	System.out.println("enter employee detail");
+
+}
+
+//data types 
+public void empDetail(String name) 
+{
+	System.out.println("enter employee detail");
+
+}
+//dat atypes count increased
+public void empDetail(String surname,int rollNum) 
+{
+	System.out.println("employee surname is"+surname);
+System.out.println("employee rollnum"+rollNum);
+
+}
+
+public static void main(String[] args) {
+	EmpData a=new EmpData();
+	a.empDetail();
+	a.empDetail("jhon");
+	a.empDetail("jaya", 4567);
+	
+	
+}
+
+
+
+}
